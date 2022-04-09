8bef2c1e7d611ce4cf16a7fad3a0d90dad1f696d
naekang <rlawlsgh6306@gmail.com>
2022-04-09 15:49:51 +0900

    Init project

---
+++ src/main/java/com/booksphillic/BooksphillicApplication.java
@@ -0,0 +1,13 @@
+package com.booksphillic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksphillicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksphillicApplication.class, args);
+	}
+
+}
