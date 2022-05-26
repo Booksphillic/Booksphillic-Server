47f2dbc360baa21d7c6f2b2b4bd212b155831916
Jinho Kim <66901361+naekang@users.noreply.github.com>
2022-05-27 02:14:30 +0900

    feat: 미스터리 리뷰 조회 api (#31)

--- src/main/java/com/booksphillic/repository/pickup/PickupReviewJpaRepository.java
+++ src/main/java/com/booksphillic/repository/pickup/PickupReviewJpaRepository.java
@@ -1,12 +1,15 @@
 package com.booksphillic.repository.pickup;
 
+import com.booksphillic.domain.pickup.Pickup;
 import com.booksphillic.domain.pickup.PickupReview;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PickupReviewJpaRepository extends JpaRepository<PickupReview, Long> {
 
     Optional<PickupReview> findById(Long reviewId);
 
+    List<PickupReview> findByPickup(Pickup pickup);
 }
