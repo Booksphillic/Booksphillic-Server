5f1b833d60853db7b98dd2f639eb8c66d47a2696
0standing2 <ysyoung1011@sookmyung.ac.kr>
2022-05-27 03:02:09 +0900

    fix: 오류 수정

--- src/main/java/com/booksphillic/repository/pickup/PickupReviewImageJpaRepository.java
+++ src/main/java/com/booksphillic/repository/pickup/PickupReviewImageJpaRepository.java
@@ -4,10 +4,13 @@ import com.booksphillic.domain.pickup.PickupReview;
 import com.booksphillic.domain.pickup.PickupReviewImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
 public interface PickupReviewImageJpaRepository extends JpaRepository<PickupReviewImage, Long> {
 
     Optional<PickupReviewImage> findById(Long id);
+
+    List<PickupReviewImage> findByPickupReview(PickupReview pickupReview);
 }
